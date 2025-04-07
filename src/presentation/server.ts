46cf44c34d12b2dcd411629fed67ff2900755d9c
Mateo Rodriguez <mdavidmontero6@gmail.com>
2025-04-07 15:49:33 -0500

    Sirviendo html

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,22 +1,26 @@
 import express, { Router } from "express";
 import cors from "cors";
 import colors from "colors";
+import path from "path";
 
 interface Options {
   port: number;
   routes: Router;
+  public_path?: string;
 }
 
 export class Server {
   private app = express();
   private readonly port: number;
   private readonly routes: Router;
+  private readonly publicPath: string;
   constructor(options: Options) {
-    const { port, routes } = options;
+    const { port, routes, public_path = "public" } = options;
     this.port = port;
+    this.publicPath = public_path;
     this.routes = routes;
   }
-JJ
+
   async start() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
@@ -27,6 +31,13 @@ JJ
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       })
     );
+    this.app.get("*", (req, res) => {
+      const indexPath = path.join(
+        __dirname + `../../../${this.publicPath}/index.html`
+      );
+      res.sendFile(indexPath);
+    });
+
     this.app.use(this.routes);
 
     this.app.listen(this.port, () => {
